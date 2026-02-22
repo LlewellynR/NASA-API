fd963361cbfa7064c89133afed265e787088d09e
ryl <ryl12@aber.ac.uk>
2026-02-22 17:41:55 +0000

    Initial commit

---
+++ src/Test.ts
@@ -0,0 +1,28 @@
+import key from './Key.js';
+async function API() {
+  const url =
+    'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key='+key;
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    const data=(document.getElementById('data') as HTMLElement);
+
+    const neo=result.near_earth_objects["2015-09-08"];
+    data.innerHTML=neo[0].is_potentially_hazardous_asteroid;
+
+    const Asteroid=document.getElementById('Ast-name') as HTMLElement;
+    Asteroid.innerHTML=neo[0].name;
+
+    const output=document.getElementById('output') as HTMLElement;
+    const link=document.createElement("a");
+    link.href=neo[0].nasa_jpl_url;
+    link.innerText="Click here for more details";
+    output.append(link);
+
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+API();
